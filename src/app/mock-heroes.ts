a2db0c22d5a0fe74c8e1e436f0bccfbae68a91f7
christinemoore <hello.christine.moore@gmail.com>
2021-02-14 09:34:31 +0200

    Initial commit

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[]= [
+    { id: 11, name: 'Dr Nice' },
+    { id: 12, name: 'Narco' },
+    { id: 13, name: 'Bombasto' },
+    { id: 14, name: 'Celeritas' },
+    { id: 15, name: 'Wesley' },
+    { id: 16, name: 'Grethe' },
+    { id: 17, name: 'Magma' },
+    { id: 18, name: 'Dr IQ' },
+    { id: 19, name: 'Theunis' },
+    { id: 20, name: 'Christine' }
+];
